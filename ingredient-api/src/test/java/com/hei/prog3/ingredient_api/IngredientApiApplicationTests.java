7e42896b84b9c013014aea17c924373d586d8080
Ange-H0610 <hei.iraka.4@gmail.com>
2026-04-15 07:04:40 +0300

    Initial commit - Spring Boot Ingredient API

---
+++ ingredient-api/src/test/java/com/hei/prog3/ingredient_api/IngredientApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hei.prog3.ingredient_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngredientApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
